c63365946356016ca7481c1a7f65f82c0ceb5a98
Stephen DiIorio <PlasmicQuark@users.noreply.github.com>
2019-11-18 11:15:36 -0500

    added iterator capabilities

--- src/GridObject.h
+++ src/GridObject.h
@@ -21,12 +21,74 @@ class GridObject
         ***********************************************************/
         GridObject(); //TODO: see if this can be removed
         GridObject(uint nx, uint ny);
-        GridObject(uint nx, uint ny, std::function<void(GridObject &, uint, uint)> init_fcn);
-        GridObject(uint nx, uint ny, std::vector<double> data); // a 'copy' constructor
+        GridObject(uint nx, uint ny,
+                   std::function<void(GridObject &, uint, uint)> init_fcn);
+        GridObject(uint nx, uint ny,
+                   std::vector<double> data); // a 'copy' constructor
         ~GridObject();
         //-----------------------------------------
 
 
+        /**********************************************************
+        ITERATOR FUNCTIONS
+        ***********************************************************/
+        inline GridObject::iterator begin() noexcept
+        {
+            return gridded_data.begin();
+        }
+        inline GridObject::const_iterator cbegin() const noexcept
+        {
+            return gridded_data.cbegin();
+        }
+        inline GridObject::iterator end() noexcept
+        {
+            return gridded_data.end();
+        }
+        inline GridObject::const_iterator cend() const noexcept
+        {
+            return gridded_data.cend();
+        }
+        //-----------------------------------------
+
+
+        /**********************************************************
+        OPERATOR FUNCTIONS
+        ***********************************************************/
+        /**
+         * @brief Overload the += operator for element-wise addition between
+         *        GridObject objects
+         *
+         */
+        inline GridObject operator+=(GridObject grid)
+        {
+            for (uint i = 0; i < this->nx * this->ny; ++i)
+            {
+                this->gridded_data.at(i) += grid.gridded_data.at(i);
+            }
+
+            return *(this);
+        }
+
+        /**
+         * @brief Overload the -= operator for element-wise subtraction between
+         *        GridObject objects
+         *
+         */
+        inline GridObject operator-=(GridObject grid)
+        {
+            for (uint i = 0; i < this->nx * this->ny; ++i)
+            {
+                this->gridded_data.at(i) -= grid.gridded_data.at(i);
+            }
+
+            return *(this);
+        }
+        //-----------------------------------------
+
+
+        /**********************************************************
+        CLASS METHODS
+        ***********************************************************/
         /**
          * @brief Add the provided value to the current value stored in the
          *        grid at a given index.
@@ -65,6 +127,7 @@ class GridObject
         }
 
         void print_grid_data();
+        //-----------------------------------------
 };
 
 #endif
