cad35786cda5cb2ebd68531eecaf6797055712f4
Stephen DiIorio <PlasmicQuark@users.noreply.github.com>
2019-11-17 19:31:17 -0500

    format and document grid object

--- src/GridObject.cpp
+++ src/GridObject.cpp
@@ -1,13 +1,23 @@
 #include "GridObject.h"
 
-
 /**********************************************************
 CONSTRUCTORS/DESTRUCTORS
 ***********************************************************/
+
+/**
+ * @brief Constructor for Grid Object:: Grid Object object
+ *
+ */
 GridObject::GridObject()
 {
 }
 
+/**
+ * @brief Constructor for Grid Object:: Grid Object object
+ *
+ * @param nx Number of x values
+ * @param ny Number of y values
+ */
 GridObject::GridObject(uint nx, uint ny)
 {
     this->nx = nx;
@@ -16,6 +26,14 @@ GridObject::GridObject(uint nx, uint ny)
     this->gridded_data = std::vector<double>(nx * ny, 0.0);
 }
 
+/**
+ * @brief Constructor for Grid Object:: Grid Object object
+ *
+ * @param nx Number of x values
+ * @param ny Number of y values
+ * @param init_fcn User provided function which initializes the data in the grid
+ *                 to the user's specification
+ */
 GridObject::GridObject(uint nx, uint ny, std::function<void(GridObject &, uint, uint)> init_fcn)
 {
     this->nx = nx;
@@ -24,40 +42,40 @@ GridObject::GridObject(uint nx, uint ny, std::function<void(GridObject &, uint, 
     init_grid_obj(init_fcn);
 }
 
+/**
+ * @brief Constructor for Grid Object:: Grid Object object
+ *
+ * @param nx Number of x values
+ * @param ny Number of y values
+ * @param data A vector object to copy the data from into the current grid
+ */
 GridObject::GridObject(uint nx, uint ny, std::vector<double> data) // a 'copy' constructor
 {
     this->nx = nx;
     this->ny = ny;
 
-    this->gridded_data = data;
+    this->gridded_data = std::vector<double>(data);
 }
 
+/**
+ * @brief Destructor for Grid Object:: Grid Object object
+ *
+ */
 GridObject::~GridObject()
 {
 }
 //-----------------------------------------
 
 
-// inline void GridObject::add_to_grid_data(uint index_x, uint index_y, double val)
-// {
-//     gridded_data.at(index_x * ny + index_y) += val;
-// }
-
-// inline void GridObject::set_grid_data(uint index_x, uint index_y, double val)
-// {
-//     gridded_data.at(index_x * ny + index_y) = val;
-// }
-
-// inline double GridObject::get_grid_data(uint index_x, uint index_y) const
-// {
-//     return gridded_data.at(index_x * ny + index_y);
-// }
-
+/**
+ * @brief Print the values stored within the grid object
+ *
+ */
 void GridObject::print_grid_data()
 {
-for (auto &f : this->gridded_data )
+    for (auto &d : this->gridded_data )
     {
-        std::cout << f << '\t';
+        std::cout << d << '\t';
     }
     std::cout << std::endl;
 }
--- src/GridObject.h
+++ src/GridObject.h
@@ -1,13 +1,13 @@
 #ifndef GRID_H
 #define GRID_H
 
-// #include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <functional>
+
 typedef unsigned int uint;
 
-class GridObject 
+class GridObject
 {
     private:
         std::vector<double> gridded_data;
@@ -26,24 +26,45 @@ class GridObject
         ~GridObject();
         //-----------------------------------------
 
-        // inline void set_grid_data(uint index_x, uint index_y, double val);
-        // inline void add_to_grid_data(uint index_x, uint index_y, double val);
-        // inline double get_grid_data(uint index_x, uint index_y) const;
+
+        /**
+         * @brief Add the provided value to the current value stored in the
+         *        grid at a given index.
+         *
+         * @param index_x x index of grid
+         * @param index_y y index of grid
+         * @param val number to sum with value at given indices
+         */
         inline void add_to_grid_data(uint index_x, uint index_y, double val)
         {
             gridded_data.at(index_x * ny + index_y) += val;
         }
 
+        /**
+         * @brief Set the grid data object
+         *
+         * @param index_x x index of grid
+         * @param index_y y index of grid
+         * @param val number to set in grid at given indices
+         */
         inline void set_grid_data(uint index_x, uint index_y, double val)
         {
             gridded_data.at(index_x * ny + index_y) = val;
         }
 
+        /**
+         * @brief Get the grid data object
+         *
+         * @param index_x x index of grid
+         * @param index_y y index of grid
+         * @return double get value stored in grid at given indices
+         */
         inline double get_grid_data(uint index_x, uint index_y) const
         {
             return gridded_data.at(index_x * ny + index_y);
         }
 
-        inline void print_grid_data();
+        void print_grid_data();
 };
+
 #endif
