9aa91a8dd4b630dc4268940e1eb4c9e4a7e85b49
RTSandberg <ryansand@umich.edu>
2019-11-25 14:12:17 -0500

    Ryan: added test_push

--- tst/test_push.cpp
+++ tst/test_push.cpp
@@ -1,11 +1,13 @@
 #include "../src/Species.h"
 
 // testing the push particles functionality of Species
+// Requires that the map_field_to_particles functionality is working
 
 int main(int argc, char** argv)
 {
 
 	Species testSpec;
+	testSpec.add
 	std::cout << "Hello world"
     return 0;
 }
