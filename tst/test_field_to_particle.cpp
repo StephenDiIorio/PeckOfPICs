fde1d4bee00865b3cf81cf86c26e2c5d5aea801c
RTSandberg <ryansand@umich.edu>
2019-11-25 14:11:55 -0500

    Ryan: renamed test files

---
+++ tst/test_field_to_particle.cpp
@@ -0,0 +1,10 @@
+#include "Species.h"
+// testing the map fields to particles functionality
+
+int main(int argc, char **argv)
+{
+    Species myspec;
+
+    
+    return 0;
+}
---
+++ tst/test_push.cpp
@@ -0,0 +1,11 @@
+#include "../src/Species.h"
+
+// testing the push particles functionality of Species
+
+int main(int argc, char** argv)
+{
+
+	Species testSpec;
+	std::cout << "Hello world"
+    return 0;
+}
